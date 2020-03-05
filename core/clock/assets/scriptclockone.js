7e6bcc3613ab8eb66deea61b19ed454c71daba06
chooj743 <chooj743@newschool.edu>
2020-03-05 14:14:37 -0500

    .

--- core/clock/assets/scriptclockone.js
+++ core/clock/assets/scriptclockone.js
@@ -1,22 +1,68 @@
-var blink_speed = 1000; // every 1000 == 1 second, adjust to suit
-var t = setInterval(function () {
-    var ele = document.getElementById('tick');
-    ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
-}, blink_speed);
 
- var source = "../core/content/Tick.mp3"
- var audio = document.createElement("audio");
 
-/*function enableLoop() { 
-  audio.loop = true;
-} 
-*/
- //
- audio.autoplay = true;
- //
- audio.load()
- audio.addEventListener("load", function() { 
-     audio.play(); 
-     audio.loop();
- }, true);
- audio.src = source;
+var text = document.getElementById("tick");
+console.log(text)
+
+var timing = [
+	[2000, 4000, 9000, 5000, 5000
+	, 5000, 5000, 5000, 5000, 5000
+	, 1000, 1000, 1000, 1000
+	, 1000, 1000, 1000, 1000, 1000, 1000, 1000 
+	, 1000, 1000, 1000, 1000, 1000],
+	[1000, 2000, 2500, 1000, 1000, 1000],
+	[1000, 2000, 2500, 1000, 1000,
+		1000, 1000, 1000, 1000, 1000, 1000,
+		1000, 1000, 1000, 1000, 1000,
+		1000, 1000, 1000, 1000, 1000, 1000, 1000,
+		1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000],
+	[1000, 2000, 2500, 1000, 1000, 1000, 1000, 1000]
+];
+
+var scenes = [
+	["alarm buzzing", "gasps", "panting", "bed creaks", "door closes",
+		"toilet flushes", "water running", "water splattering","water stops", "door opens", 
+		"remote clicks", "static noise", "TV plays in the background", "indistinct chatter over TV", 
+		"footsteps", "dishes clatter", "yawns", "fridge door opens and closes", "utensils clank", "remote clicks", "thud", 
+		"floor creaks", "keys rattling", "zip fastening", "shoes thud", "doors slams"
+	],
+	["silence", "silence continues", "horn honks in distance", "truck reversing in distance", "silence", "silence continues"],
+	["footsteps in distance", "key in lock", "doors open", "shoes thud", "keys rattling",
+		"switch ticks", "door closes", "bag rustles", "laptop chimes", "music playing", "typing",
+		"door buzz rings", "inaudible", "knock on door", "lock clicks", "door squeaks open",
+		"objects clanging", "music stops abruptly", "glasses shattering", "breathing heavily", "floor creaking", "breahe shakily", "device dialing",
+		"footsteps approaching quickens", "door opens loudly", "painting", "gun cocks", "muffled cries", "chuckles", "gunshots", "thud", "silence"
+	],
+	["silence continues", "siren wails in distance", "screaming", "people shouting", "silence", "silence continues", "birds chirping", "birds chirping continues"]
+];
+
+
+
+function subtitleContent(sceneIndex, actionIndex, subtitleELementA){
+	subtitleELementA.innerHTML = "[" + scenes[sceneIndex][actionIndex] + "]"
+}
+
+
+text.innerHTML = "[" + "tick" + "]";
+
+
+
+var currentScene = 0;
+var currentAction = 0;
+
+setInterval(function(){
+
+	subtitleContent(currentScene, currentAction, text)
+
+	if (currentAction < timing[currentScene].length - 1){
+
+		currentAction = currentAction + 1;
+
+	}else{
+		currentAction = 0;
+		currentScene = currentScene + 1;
+	}
+
+}, timing[currentScene][currentAction])
+
+
+
---
+++ lab/mr/assets/scriptmodrep.js
@@ -0,0 +1,31 @@
+var myRootDiv = document.getElementById("square")
+console.log("myRootDiv")
+
+myRootDiv.insertAdjacentHTML('beforeend', `
+		<div class="box-1"></div>
+	`)
+
+var degrees = 0;
+
+
+	for (var i = 0; i < 50; i++) {
+
+		var r = Math.ceil(Math.random()*255) + 130;
+		var g = Math.round(Math.random()*255) + 160;
+		var b = Math.floor(Math.random()*255) + 120;
+
+		
+		myRootDiv.insertAdjacentHTML('beforeend', `
+			<div 
+				class="box-1"
+				style="transform:rotate(${degrees}deg);
+						background-color: rgb(${r}, ${g}, ${b});"
+			></div>
+		`)
+
+		degrees = degrees + 25;
+
+	}
+
+
+
