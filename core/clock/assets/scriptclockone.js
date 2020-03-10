f1c4db62e5aa7f3b7a8c5cf82fb229dbe4a0d2d1
chooj743 <chooj743@newschool.edu>
2020-03-10 00:01:48 -0400

    Update scriptclockone.js

--- core/clock/assets/scriptclockone.js
+++ core/clock/assets/scriptclockone.js
@@ -13,62 +13,66 @@ var globalCurrentAction = 0; // corresponds to each item in the subArray
 
 var ourHours = myDate.getHours(); //0 - 23
 var ourMinutes = myDate.getMinutes(); //0 - 59
+console.log(ourHours)
 
 
+//STARTING CHECKPOITNS
 //scene 1: Morning 6:30 till 8:00
-if(ourHours === 5 && ourMinutes === 29){
+if(ourHours === 6 && ourMinutes === 30){
 	globalCurrentScene = 0;
 	globalCurrentAction = 0;
 }
 
 //scene 2: RUSH HOUR 8:00 till 10:00
-if(ourHours === 7 && ourMinutes === 9 ){
+if(ourHours === 8 && ourMinutes === 10){
 	globalCurrentScene = 1;
 	globalCurrentAction = 0;
 }
 
 //scene 3: STILL 11:30 till 14:00
-if(ourHours === 10 && ourMinutes > 24){
+if(ourHours >= 11 && ourMinutes > 23){
 	globalCurrentScene = 2;
 	globalCurrentAction = 0;
 }
 
 //scene 4: RUSH HOUR 15:00 till 18:00
-if(ourHours === 10 && ourMinutes > 30){
+if(ourHours === 15){
 	globalCurrentScene = 3;
 	globalCurrentAction = 0;
 }
 
 //scene 5: ACTIVE 18:00 till 19:00
-if(ourHours === 10 && ourMinutes > 30){
+if(ourHours === 18){
 	globalCurrentScene = 4;
 	globalCurrentAction = 0;
 }
 
 //scene 6: ACTION 19:00 till 20:00
-if(ourHours === 10 && ourMinutes > 30){
+if(ourHours === 19){
 	globalCurrentScene = 5;
 	globalCurrentAction = 0;
 }
 
 //scene 7: AFTER ACTION 21:00 till 24:00
-if(ourHours === 10 && ourMinutes > 30){
+if(ourHours === 20){
 	globalCurrentScene = 6;
 	globalCurrentAction = 0;
 }
 
 //scene 8: STILL 5:00 till 
-if(ourHours === 10 && ourMinutes > 30){
+if(ourHours === 21){
 	globalCurrentScene = 7;
 	globalCurrentAction = 0;
 }
 
 
+
+//TIMING OF PAIRED SCENES
 var timing = [
 	[8000, 7000, 5000, 40000, 50000, 10000,
 		600000, 50000, 5000, 180000, 1851000, 50000,
 		10000, 13000, 4000, 10000,
-		30000, 3000, 10000, 10000, 30000,
+		30000, 5000, 10000, 10000, 30000,
 
 		5000, 180000, 900000, 30000,
 		120000, 300000, 300000, 5000, 20000,
@@ -85,6 +89,7 @@ var timing = [
 	[2000, 2500, 1000, 1000, 1000, 1000, 1000]
 ];
 
+//SUBTITLE TEXTS
 var scenes = [
 	["alarm buzzing", "alarm buzzing continues", "gasps", "panting", "bed creaks", "door closes",
 		"faucet running", "toilet flushes", "glassware tinkling", "faucet stops running", "water splattering", "door opens", 
@@ -113,22 +118,24 @@ function subtitleContent(sceneIndex, actionIndex, subtitleELementA){
 	console.log("action: ", scenes[sceneIndex][actionIndex])
 	subtitleELementA.innerHTML = "[" + scenes[sceneIndex][actionIndex] + "]"
 
+	//EACH SUBTITLE TO APPEAR FOR 5s DURATION
 	if(timing[sceneIndex][actionIndex] > 5000){
 		setTimeout(function(){
 			subtitleELementA.innerHTML = ''
 		}, 5000)
 	}
 
+	//TICKING WHEN SILENCE FOR MORE > 10s
 	if(timing[sceneIndex][actionIndex] > 10000){
 		
 		// get total amount of time before next action
 		var countDown = timing[sceneIndex][actionIndex];
-
+		console.log("before " + countDown)
 		// after a delay of 9 seconds, start ticking
 		setTimeout(function(){
 			// immediatly subtract the delay from our countdown (we want to get to 0 to know when to stop)
 			countDown = countDown - 9000;
-			
+			console.log("after " + countDown)
 			var showTick = true // the showTick toggles between showing and hiding the ticker
 			// tick every second:
 			var ticking = setInterval(function(){
@@ -139,7 +146,6 @@ function subtitleContent(sceneIndex, actionIndex, subtitleELementA){
 					// if our countdown has reached 0, stop ticking
 					clearInterval(ticking)
 				}
-
 				// if showTick is true, show our ticker and swithc to false for next time
 				if(showTick){
 					subtitleELementA.innerHTML = "[" + "tick" + "]"
@@ -155,6 +161,8 @@ function subtitleContent(sceneIndex, actionIndex, subtitleELementA){
 }
 
 
+
+//ENABLES SCENES TO FIRE IN ORDER
 function delayedTime(currentScene, currentAction){
 	// update our subtitle
 	console.log("timing: ", timing[currentScene][currentAction])
